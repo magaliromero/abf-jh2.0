81e4adc8bf20083d5764b9ee7863d85a96d15343
Marta H <martitah-1@fpuna.edu.py>
2023-11-02 22:03:27 -0300

    validación agregada en préstamo
    validación agregada en préstamo
    cambio en validación de stock de materiales

--- src/main/webapp/app/entities/prestamos/update/prestamos-update.component.ts
+++ src/main/webapp/app/entities/prestamos/update/prestamos-update.component.ts
@@ -27,6 +27,8 @@ export class PrestamosUpdateComponent implements OnInit {
 
   editForm: PrestamosFormGroup = this.prestamosFormService.createPrestamosFormGroup();
 
+  hayStock = true;
+
   constructor(
     protected prestamosService: PrestamosService,
     protected prestamosFormService: PrestamosFormService,
@@ -48,6 +50,13 @@ export class PrestamosUpdateComponent implements OnInit {
 
       this.loadRelationshipsOptions();
     });
+  }
+  verificarStock() {
+    let data = this.materialesSharedCollection.find(item => item.id == this.editForm.controls.materiales.value?.id);
+    console.log(data);
+    if ((data?.cantidad ?? 0) < 1) {
+      this.hayStock = false;
+    }
   }
 
   previousState(): void {
